c23c29b774f59f2137a79706d129c5641c6ce45e
Ritesh078bct <078bct068@student.ioepc.edu.np>
2024-02-03 23:59:16 +0545

    added UI for the profile page

--- frontend/realstate/src/App.jsx
+++ frontend/realstate/src/App.jsx
@@ -5,7 +5,7 @@ import SignUp from "./pages/SignUp.jsx";
 import About from "./pages/About.jsx";
 import Profile from "./pages/Profile.jsx";
 import Header from "./components/Header.jsx";
-import PrivateRoute from "./components/PrivateRoute.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx"
 
 
 export default function App() {
--- frontend/realstate/src/pages/Profile.jsx
+++ frontend/realstate/src/pages/Profile.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import{useSelector} from "react-redux"
 
 export default function Profile() {
+  const {currentUser}=useSelector((state)=>state.user)
   return (
-    <div>Profile</div>
+    <div className="mt-8 max-w-lg mx-auto px-3 flex flex-col justify-center items-center gap-2">
+      <h1 className="text-center font-bold text-3xl">Profile</h1>
+      <img src={currentUser.avatar} alt="profile" className="rounded-full h-20 w-20 object-cover "/>
+      <form >
+        <input placeholder="username" type="text" className="p-3 my-1 rounded-lg w-[100%] " />
+        <input placeholder="email" type="text" className="p-3 my-1 rounded-lg w-[100%] " />
+        <input placeholder="password" type="text" className="p-3 my-1 rounded-lg w-[100%] " />
+      </form>
+      <button className="bg-slate-700 text-white w-[100%] p-3 rounded-lg hover:opacity-85 ">Update</button>
+      <div className="p-3 text-red-700 font-semibold  w-[100%] flex justify-between">
+        <span className="hover:cursor-pointer">Delete Account</span>
+        <span className="hover:cursor-pointer">log out</span>
+      </div>
+    </div>
   )
 }
